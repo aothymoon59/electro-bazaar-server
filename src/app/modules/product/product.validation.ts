579ae03980338c510eb7881a5c00ad9f4980d603
aothymoon59 <aothymoon59@gmail.com>
2024-10-02 11:58:05 +0600

    [ADD] update product validation schema added

--- src/app/modules/product/product.validation.ts
+++ src/app/modules/product/product.validation.ts
@@ -21,5 +21,29 @@ const createProductValidationSchema = z.object({
       .optional(),
   }),
 });
+const updateProductValidationSchema = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    productImage: z.string().optional(),
+    price: z.number().optional(),
+    quantity: z.number().optional(),
+    releaseDate: z.string().optional(),
+    brand: z.string().optional(),
+    modelNumber: z.string().optional(),
+    category: z.string().optional(),
+    operatingSystem: z.string().optional(),
+    connectivity: z.string().optional(),
+    powerSource: z.string().optional(),
+    features: z
+      .object({
+        cameraResolution: z.number().optional(),
+        storageCapacity: z.number().optional(),
+      })
+      .optional(),
+  }),
+});
 
-export const productValidations = { createProductValidationSchema };
+export const productValidations = {
+  createProductValidationSchema,
+  updateProductValidationSchema,
+};
