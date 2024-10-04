14abf9bfc0da4f4c7fc30b2e2ef47ee3bf47db24
aothymoon59 <aothymoon59@gmail.com>
2024-10-04 17:26:07 +0600

    [ADD] cget all managers and super admins service

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -3,6 +3,26 @@ import QueryBuilder from '../../builder/QueryBuilder';
 import { User } from '../user/user.model';
 import { USER_ROLE, userSearchableFields } from './user.constant';
 
+// TODO: need to fix getAllManagersAndSuperadminsFromDb function
+const getAllManagersAndSuperadminsFromDb = async (query: any) => {
+  const userQuery = new QueryBuilder(
+    User.find({ role: { $in: [USER_ROLE.manager, USER_ROLE.superAdmin] } }),
+    query,
+  )
+    .search(userSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await userQuery.modelQuery;
+  const meta = await userQuery.countTotal();
+  return {
+    result,
+    meta,
+  };
+};
+
 const getAllUsersFromDb = async (query: any) => {
   const userQuery = new QueryBuilder(
     User.find({ role: { $in: [USER_ROLE.user, USER_ROLE.customer] } }),
@@ -29,6 +49,7 @@ const getMe = async (email: string, role: string) => {
 };
 
 export const UserServices = {
+  getAllManagersAndSuperadminsFromDb,
   getAllUsersFromDb,
   getMe,
 };
