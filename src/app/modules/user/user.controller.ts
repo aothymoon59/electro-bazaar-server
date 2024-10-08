653669ff296530411323c8eee36853133bf2c1d8
aothymoon59 <aothymoon59@gmail.com>
2024-10-08 22:35:16 +0600

    [ADD] changeStatus controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -40,8 +40,22 @@ const getMe = catchAsync(async (req, res) => {
   });
 });
 
+const changeStatus = catchAsync(async (req, res) => {
+  const id = req.params.id;
+
+  const result = await UserServices.changeStatus(id, req.body);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Status is updated successfully',
+    data: result,
+  });
+});
+
 export const UserController = {
   getAllManagersAndSuperadmins,
   getAllUsers,
   getMe,
+  changeStatus,
 };
