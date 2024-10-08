98769c180c2c3b9339e321e87a211d9c77e848ac
aothymoon59 <aothymoon59@gmail.com>
2024-10-08 23:11:32 +0600

    [ADD] changeRole Service

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -54,9 +54,17 @@ const changeStatus = async (id: string, payload: { status: string }) => {
   return result;
 };
 
+const changeRole = async (id: string, payload: { role: string }) => {
+  const result = await User.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+  return result;
+};
+
 export const UserServices = {
   getAllManagersAndSuperadminsFromDb,
   getAllUsersFromDb,
   getMe,
   changeStatus,
+  changeRole,
 };
