2ee432172729c9ffc6ebd07127daf8496cc79404
aothymoon59 <aothymoon59@gmail.com>
2024-10-09 23:48:23 +0600

    [ADD] cart interface

---
+++ src/app/modules/cart/cart.interface.ts
@@ -0,0 +1,9 @@
+export interface ICart {
+  product_id: string;
+  email: string;
+  product_name: string;
+  product_image: string;
+  price: number;
+  quantity: number;
+  product_quantity: number;
+}
